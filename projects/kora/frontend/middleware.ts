903c2099828bda069f54c832b09eff3fa4dc4f7b
Thuy Nguyen <tttn.tn13@gmail.com>
2025-04-22 15:45:38 +0000

    changed the path

--- projects/kora/frontend/middleware.ts
+++ projects/kora/frontend/middleware.ts
@@ -1,5 +1,4 @@
-// import type { NextRequest } from "next/server"
-import { NextRequest } from "/home/thuy/monorepo/projects/node_modules/next/dist/server/web/spec-extension/request";
+import type { NextRequest } from "next/server"
 
 import { auth0 } from "./lib/auth0"
 
